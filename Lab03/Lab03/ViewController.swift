ba39dacdb691fd8dc4c988d3e9fb7ef074f56b53
Mario Powell <s00884133@acad.tri-c.edu>
2021-01-26 11:21:55 -0500

    Lab03

---
+++ Lab03/Lab03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab03
+//
+//  Created by Mario Powell on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab03/Lab03Tests/Lab03Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Lab03Tests.swift
+//  Lab03Tests
+//
+//  Created by Mario Powell on 1/26/21.
+//
+
+import XCTest
+@testable import Lab03
+
+class Lab03Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
